0a43f8af87fc0b7d7d84862ea158f41644da0501
jamalalqale <libyan.joey@gmail.com>
2023-12-27 11:14:21 +0100

    Update MQ135_Calibration.ino

--- MQ135/MQ135_Calibration/MQ135_Calibration.ino
+++ MQ135/MQ135_Calibration/MQ135_Calibration.ino
@@ -3,10 +3,9 @@
 #define placa "Arduino UNO"
 #define Voltage_Resolution 5
 #define pin A0 //Analog input 0 of your arduino
-#define type "MQ-135" //MQ135
-#define ADC_Bit_Resolution 10 // For arduino UNO/MEGA/NANO
-#define RatioMQ135CleanAir 3.6//RS / R0 = 3.6 ppm  
-//#define calibration_button 13 //Pin to calibrate your sensor
+#define type "MQ-135" 
+#define ADC_Bit_Resolution 10 
+#define RatioMQ135CleanAir 3.6
 
 //Declare Sensor
 MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
@@ -15,7 +14,7 @@ void setup() {
   Serial.begin(9600); 
   MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b 
   MQ135.init(); 
-  MQ135.setRL(1);
+  MQ135.setRL(1);// set RL
 }
 
 void loop() {
