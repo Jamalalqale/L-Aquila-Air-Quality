125a5c41fa5db33ec3b845efdf2799044117fb9c
jamalalqale <libyan.joey@gmail.com>
2023-12-27 13:06:07 +0100

    Update MQ7Read.ino

--- MQ7/MQ7Read/MQ7Read.ino
+++ MQ7/MQ7Read/MQ7Read.ino
@@ -17,7 +17,7 @@ void setup() {
   MQ7.setRegressionMethod(1); //_PPM =  a*ratio^b
   MQ7.setA(99.042); MQ7.setB(-1.518); // Configure the equation to calculate CO concentration value
   MQ7.setR0(3.3);//set R0
-  MQ7.setRl(1);//set Rl
+  MQ7.setRL(1);//set Rl
   /*
     Exponential regression:
   GAS     | a      | b
@@ -26,36 +26,31 @@ void setup() {
   CH4     | 60000000000000 | -10.54
   CO      | 99.042 | -1.518
   Alcohol | 40000000000000000 | -12.35
-  */
-  MQ7.init(); 
-  //prepare
-// 60s cycle
-  oldTime = millis();
-  while(millis() - oldTime <= (60*1000))
-  {
-    // VH 5 Volts
-    analogWrite(5, 255); // 255 is DC 5V output
-    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
-    MQ7.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-    MQ7.serialDebug(); // Will print the table on the serial port
-    delay(500); //Sampling frequency
-  }
-
-  // 90s cycle
-  oldTime = millis();
-  while(millis() - oldTime <= (90*1000))
-  {
-    // VH 1.4 Volts
-    analogWrite(5, 20); // 255 is 100%, 20.4 is aprox 8% of Duty cycle for 90s
-    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
-    MQ7.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-    MQ7.serialDebug(); // Will print the table on the serial port
-    delay(500); //Sampling frequency
-  } 
-  // Total: 60 + 90s =  2.5 minutes.
-
+  */  
+  MQ7.init();   
+   
 }
 
 void loop() {
-  
+  /*
+   the sensor must be 'heated' at 5 V for 60 s to desorb the gases, then powered at 1.4 V for 90 s during the actual reading.
+   According to MQ-7 datasheet, sensor has to run through high- and low-heating cycles in order to get proper measurements. 
+   During low temperature phase, CO is absorbed on the plate, producing meaningful data.
+  During high temperature phase, absorbed CO and other compounds evaporate from the sensor plate, cleaning it for the next measurement.
+  */  
+ unsigned long startTime = millis(); // Record the start time
+  while (millis() - startTime <= 60000) {  // Run for 60 seconds
+    analogWrite(5, 255); // 255 is DC 5V output
+    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
+    MQ7.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup  
+    delay(500); //Sampling frequency
+  }
+   startTime = millis(); // Record the start time
+    while (millis() - startTime <= 90000) {  // Run for 90 seconds
+    analogWrite(5, 20); // 255 is DC 5V output
+    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
+    MQ7.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
+    MQ7.serialDebug(); // Will print the table on the serial port
+    delay(500); //Sampling frequency
+  }  
 }
